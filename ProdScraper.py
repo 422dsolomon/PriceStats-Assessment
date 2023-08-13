ed810a5e50361889bbe038d71f6fefa543154c14
422dsolomon <43157672+422dsolomon@users.noreply.github.com>
2023-08-12 21:02:56 -0400

    Added functions to connect to sqlite3, create a table and main

--- ProdScraper.py
+++ ProdScraper.py
@@ -22,5 +22,31 @@ def create_table(conn, create_table_sql):
         print(f"Error cannot create table")
         os.sys.exit(1)
 
+
 def main():
+    #Database Path
+    db_path = "./prod_sqlite.db"
+
+    #Connect to the Database 
+    conn = sqlConnect(db_path)
+
+    #Instantiate cursor
+    db_cursor = conn.cursor()
+
+    #Create Table
+    #Table Fields: Unique product identifier, Product name, Product description (when available), 
+    # Price, Sale price (when available), Out of stock indicator (when available), 
+    # URL the product was captured from
+    sql_create_table = """CREATE TABLE IF NOT EXISTS Product_Pricing_Data (product_id interger, 
+                            product_name text, product_desc text, price interger, 
+                            sale_price interger, OOSI boolean, URL text);"""
     
+    if conn is not None:
+        create_table(conn, sql_create_table)
+    else:
+        print("Error! cannot create the database connection")
+
+    
+    if __name__ == "__main__":
+        main()
+
