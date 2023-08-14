5b76c08b35735395a2d4e923dd87fcb2085e0a1e
422dsolomon <43157672+422dsolomon@users.noreply.github.com>
2023-08-13 21:06:51 -0400

    Making look cleaner

--- ProdScraper.py
+++ ProdScraper.py
@@ -81,19 +81,14 @@ def scrapeProducts(page):
 def OrderAndCSV():
     #Database Path
     db_path = "./prod_sqlite.db"
-
     #Connect to database
     conn = sqlConnect(db_path)
-
     #Instantiate cursor
     db_cursor = conn.cursor()
-
     # Order
     db_cursor.execute("""SELECT * FROM Product_Pricing_Data ORDER BY COALESCE(reg_price, sale_price) DESC, product_name""")
-
     #Commit
     conn.commit()
-
     #print out CSV
     for i in db_cursor:
         print(i)
